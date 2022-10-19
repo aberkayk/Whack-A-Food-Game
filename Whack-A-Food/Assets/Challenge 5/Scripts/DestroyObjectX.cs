d100a2d808612a905159499774f8d998c2fbfdf7
Ahmet Berkay <aberkayk@gmail.com>
2022-10-19 14:38:39 +0300

    Whack-A-Food-Game

---
+++ Whack-A-Food/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+
+    private GameManager gameManager;
+
+
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
